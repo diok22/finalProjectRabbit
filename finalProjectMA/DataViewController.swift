adfeaf59484a49c0282b04d308bbfd1af13cc06b
Dionysis <Dionysis.k22@gmail.com>
2016-12-06 10:37:56 +0000

    initial commit, xcode project

---
+++ finalProjectMA/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  finalProjectMA
+//
+//  Created by Dionysis Kastellanis on 06/12/2016.
+//  Copyright © 2016 MAfinalProjectGroup. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
