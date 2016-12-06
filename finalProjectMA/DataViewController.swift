12429ca65a95291a4b6c1d890ea140232dabd8f4
Ed Powderham <edpowderham@outlook.com>
2016-12-06 12:36:10 +0000

    Adding a second pin to the map

--- finalProjectMA/DataViewController.swift
+++ finalProjectMA/DataViewController.swift
@@ -25,10 +25,17 @@ class DataViewController: UIViewController {
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.position = CLLocationCoordinate2D(latitude: 51.517467, longitude: -0.073259)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
+        
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2D(latitude: 50.517467, longitude: -0.073259)
+        marker2.icon = GMSMarker.markerImage(with: .blue)
+        marker2.title = "Sydney2"
+        marker2.snippet = "Australia2"
+        marker2.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
