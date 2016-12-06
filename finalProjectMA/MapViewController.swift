7e6a2c04bab780e570dd3415471409e7ed67a130
Dionysis <Dionysis.k22@gmail.com>
2016-12-06 17:38:40 +0000

    creates button to events on map, map is within a container

--- finalProjectMA/MapViewController.swift
+++ finalProjectMA/MapViewController.swift
@@ -11,47 +11,10 @@
     
     class MapViewController: UIViewController {
         
-    
-        let users : [[String:Any]] =
-            [
-                ["name": "Ed", "latitude": 51.55, "longitude": -0.073259],
-                ["name": "Dio", "latitude": 51.50, "longitude": -0.070],
-                ["name": "Manu", "latitude": 51.51, "longitude": -0.071]
-        ]
         
         
         override func viewDidLoad() {
-            // Create a GMSCameraPosition that tells the map to display the
-            // coordinate -33.86,151.20 at zoom level 6.
-            let camera = GMSCameraPosition.camera(withLatitude: 52.86, longitude: 0.0, zoom: 6.0)
-            let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-            mapView.isMyLocationEnabled = true
-            view = mapView
-            
-            for i in 0 ..< users.count {
-                let marker = GMSMarker()
-                marker.position = CLLocationCoordinate2D(latitude: users[i]["latitude"] as! CLLocationDegrees, longitude: users[i]["longitude"] as! CLLocationDegrees)
-                marker.title = users[i]["name"] as! String?
-                marker.snippet = "User"
-                marker.map = mapView
-            }
-            
-            let marker = GMSMarker()
-            marker.position = CLLocationCoordinate2D(latitude: 51.5014, longitude: -0.1419)
-            marker.title = "Buckingham Palace"
-            marker.snippet = "tour"
-            marker.icon = GMSMarker.markerImage(with: .blue)
-            marker.map = mapView
-            
-        }
-        
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
+            super.viewDidLoad()
         }
         
     }
