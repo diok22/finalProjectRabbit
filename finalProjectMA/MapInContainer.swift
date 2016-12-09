bdb29528c8a85e8bd2c353958e391674e9155773
Dionysis <Dionysis.k22@gmail.com>
2016-12-09 21:15:34 +0000

    removes unnecessary lines in mapviewcontroller

--- finalProjectMA/MapInContainer.swift
+++ finalProjectMA/MapInContainer.swift
@@ -13,45 +13,42 @@ import SwiftyJSON
 import FirebaseDatabase
 
 class MapInContainer: UIViewController, CLLocationManagerDelegate  {
-    
+
     var passedSelectedEventFromList: [Event] = []
-    
-    let ref = FIRDatabase.database().reference(withPath: "users")
-    
     var userFromFirebase : [String:Any] = [:]
-    
+
+    let ref = FIRDatabase.database().reference(withPath: "users")
+
     override func viewDidLoad() {
-        
+
         let meetingTime = self.passedSelectedEventFromList[0].time
         let fullAddress = self.passedSelectedEventFromList[0].address
         let lat = NSString(string: self.passedSelectedEventFromList[0].latitude).doubleValue
         let lng = NSString(string: self.passedSelectedEventFromList[0].longitude).doubleValue
-        
-     
+
+
         ref.observe(.value, with:{ snapshot in
-            
+
             let enumerator = snapshot.children
-            
+
             while let user = enumerator.nextObject() as? FIRDataSnapshot {
                 var userValue = user.value as! [String:AnyObject]
                 self.userFromFirebase["name"] = (userValue["name"] as AnyObject?)
                 self.userFromFirebase["latitude"] = (userValue["latitude"] as AnyObject?)
                 self.userFromFirebase["longitude"] = (userValue["longitude"] as AnyObject?)
                 self.userFromFirebase["eta"] = (userValue["eta"] as AnyObject?)
-                
+
                 let urlAPI = "https://maps.googleapis.com/maps/api/directions/json?"
                 let urlKey = "key=AIzaSyDEw43MvKypSnZOmxMiTzXs4nJ0ZsTjyJo"
                 let latString = String(describing: self.userFromFirebase["latitude"]!)
                 let lonString = String(describing: self.userFromFirebase["longitude"]!)
                 let eventLatString = self.passedSelectedEventFromList[0].latitude
                 let eventLonString = self.passedSelectedEventFromList[0].longitude
-                
                 let urlLocation = "origin=" + latString + "," + lonString + "&"
                 let urlDestination = "destination=" + eventLatString! + "," + eventLonString! + "&"
                 let urlTransit = "mode=transit&"
                 let url = urlAPI + urlLocation + urlTransit + urlDestination + urlKey
-                
-                
+
                 Alamofire.request(url).responseJSON
                     { response in
                         switch response.result {
@@ -65,45 +62,42 @@ class MapInContainer: UIViewController, CLLocationManagerDelegate  {
                             let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
                             mapView.isMyLocationEnabled = true
                             self.view = mapView
-                            
+
                             let userlat = self.userFromFirebase["latitude"]!
                             let userlon = self.userFromFirebase["longitude"]!
-                            
+
                             //for i in 0 ..< snapshot.childrenCount {
-                           
+
                             let marker = GMSMarker()
                             marker.position = CLLocationCoordinate2D(latitude: userlat as! CLLocationDegrees, longitude: userlon as! CLLocationDegrees)
-                            
+
                             marker.title = self.userFromFirebase["name"] as! String?
                             marker.snippet = self.userFromFirebase["eta"] as! String?
                             marker.map = mapView
-                           
-                            
+
                             let markerEvent = GMSMarker()
                             markerEvent.position = CLLocationCoordinate2D(latitude: lat, longitude: lng)
                             markerEvent.title = fullAddress
                             markerEvent.snippet = meetingTime
                             markerEvent.icon = GMSMarker.markerImage(with: .blue)
                             markerEvent.map = mapView
-                            
+
                         case .failure(let error):
                             print(error)
                         }
                      }
-                  }
+            }
         })
-        
 
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
+
 }
